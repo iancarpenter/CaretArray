151441ae7ac0ccf0a04841f4ec15e1cfc099479f
iancarpenter <ianhcarpenter@gmail.com>
2022-10-17 19:35:53 +0100

    Add project files.

---
+++ CaretArray/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CaretArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3 };
+            numbers = AddToArray(numbers, 4);
+
+            // use the index from end operator ^
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-8.0/ranges#systemindex
+            Console.WriteLine($"The last number in the array in {numbers[^1]}");
+        }
+
+        static int[] AddToArray(int[] input, int newNumber)
+        {
+            int[] output = new int[input.Length + 1];
+
+            for(int index = 0; index < input.Length; index++)
+            {
+                output[index] = input[index];                
+            }
+             
+            output[^1] = newNumber;
+
+            return output;
+        }
+    }
+}
